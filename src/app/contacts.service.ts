3f2f85b8539bac6198b8bab35aa989a3e42206ce
denysLerroan <lerroandeco@gmail.com>
2020-10-22 23:25:40 -0300

    ContactService with Http methods

---
+++ src/app/contacts.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Contact } from './contact';
+
+const urlBase = 'http://localhost:3000';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContactsService {
+  constructor(private http: HttpClient) {}
+
+  listContact() {
+    return this.http.get<Contact[]>(`${urlBase}/contacts`);
+  }
+  retrieveContact(id: number) {
+    return this.http.get<Contact>(`${urlBase}/contacts/${id}`);
+  }
+
+  createContact(contact: Contact) {
+    return this.http.post<Contact>(`${urlBase}/contacts`, contact);
+  }
+
+  updateContact(contact: Contact) {
+    return this.http.put<Contact>(`${urlBase}/contacts/${contact.id}`, contact);
+  }
+
+  deleteContact(id: number) {
+    return this.http.delete(`${urlBase}/contacts/${id}`);
+  }
+}
