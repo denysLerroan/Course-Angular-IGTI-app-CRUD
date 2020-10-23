1d64eb0aed9cb43e96ebf5f0f62cc5235bd4ae2f
denysLerroan <lerroandeco@gmail.com>
2020-10-22 23:38:27 -0300

    Using the listContact() method (ContactService) to display the contact list

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact';
+import { ContactsService } from '../contacts.service';
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  styleUrls: ['./contact-list.component.css'],
 })
 export class ContactListComponent implements OnInit {
-
-  constructor() { }
+  contacts: Contact[] = [];
+  constructor(private contactService: ContactsService) {}
 
   ngOnInit(): void {
+    this.contactService.listContact().subscribe((contacts) => {
+      this.contacts = contacts;
+    });
   }
-
 }
