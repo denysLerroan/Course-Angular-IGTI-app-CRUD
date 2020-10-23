8a3723036759396e73099ac96c9efee82c11ddba
denysLerroan <lerroandeco@gmail.com>
2020-10-22 23:54:14 -0300

    ContactCreate functionality was implemented

--- src/app/contact-create/contact-create.component.ts
+++ src/app/contact-create/contact-create.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ContactsService } from '../contacts.service';
 
 @Component({
   selector: 'app-contact-create',
   templateUrl: './contact-create.component.html',
-  styleUrls: ['./contact-create.component.css']
+  styleUrls: ['./contact-create.component.css'],
 })
-export class ContactCreateComponent implements OnInit {
+export class ContactCreateComponent {
+  contactForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    phone: new FormControl('', Validators.required),
+  });
 
-  constructor() { }
+  constructor(
+    private contactsService: ContactsService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  createContact(): void {
+    const newContact = this.contactForm.value;
+    this.contactsService.createContact(newContact).subscribe((contact) => {
+      this.router.navigate(['/contacts']);
+    });
   }
-
 }
